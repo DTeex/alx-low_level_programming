b112acaebcbfd6bc6480c5c5575eb6bb540f57d9
Chukwudubem <nwajaguchidubem@yahoo.com>
2023-10-19 12:49:08 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,10 +16,13 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str == NULL)
+		{
 			printf("[0] (nil)\n");
 
-		else
+		}else
+		{
 			printf("[%d] %s\n", h->len, h->str);
+		}
 
 		nodes++;
 		h = h->next;
